71850976f9c02feae6243bdf9a5092a1bcc12991
cgrkhrmn <ckahramanqa@gmail.com>
2017-09-08 17:53:03 -0400

    update on cukes runner

--- src/test/java/com/lenova/project/runners/CukesRunner.java
+++ src/test/java/com/lenova/project/runners/CukesRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		plugin = "json:target/cucumber-reports",
+		plugin = {"html:target/cucumber-reports", "json:target/cucumber.json"},
 		features = "./src/test/resources/features/",
 		glue = "com.lenova.project.stepDefinitions",
 		tags = "@lenova",
